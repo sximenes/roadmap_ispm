1113918e10fd43c86309f251e60619318ef126cd
Marcius Oliveira <marcius.programador@gmail.com>
2012-08-02 17:30:58 -0300

    Removendo pasta test e criado test para value de feature do produto

--- produtospec/models.py
+++ produtospec/models.py
@@ -1,33 +1,69 @@
 from django.db import models
 
-#Default configs
-DECIMAL = {'max_digits':5, 'decimal_places':2}
-
 
 class Product(models.Model):
     '''
     Classe que define um produto.
+    ex. {
+            'Camiseta MegaBoga',
+            23,00,
+            <<camiseta>>
+        }
     '''
     name = models.CharField(max_length=200)
-    price = models.DecimalField(**DECIMAL)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     product_spec = models.ForeignKey('ProductSpec')
+
+    def feature_is_valid(self, feature):
+        '''
+        Recebe uma feature e verifica se o product_spec do produto e o mesmo
+        da feature passada
+        '''
+        valid_features = Feature.objects.filter(product_spec=self.product_spec)
+        if feature.product_spec != self.product_spec:
+            return False
+        return True
 
 
 class ProductSpec(models.Model):
     '''
-    Classe que define um especifical de produto
+    Classe que define um tipo/especificao de produto
+    ex.
+        {
+            'camiseta'
+        }
     ''' 
     name = models.CharField(max_length=200)
  
 
 class Feature(models.Model):
+    '''
+    Classe que define uma caracteristica de uma especificacao de produto
+    ex.
+        {
+            'Cor',
+            'Define a cor do produto',
+            <<camiseta>>
+        }
+    '''
     name = models.CharField(max_length=200)
     description = models.TextField()
     product_spec = models.ForeignKey('ProductSpec')
 
 
 class FeatureValue(models.Model):
-    value = models.DecimalField(**DECIMAL)
+    '''
+    Classe que define o valor de uma determinada caracteristica de produto em um produto especifico
+    ex.
+        {
+            'Vermelho',
+            <<cor>>,
+            <<Camiseta MegaBoga>>
+        }
+    
+    obs. As features escolhidas devem pertencer ao grupo de features do produto_spec do produto
+    '''
+    value = models.CharField(max_length=200)
     feature = models.ForeignKey('Feature')
     product = models.ForeignKey('Product')
 
---
+++ produtospec/tests.py
@@ -0,0 +1,84 @@
+#python
+from decimal import Decimal
+#django
+import unittest
+#project
+from produtospec.models import *
+
+
+class ProductTestCase(unittest.TestCase):
+    '''
+    Conter todos os testes do modelo Product
+    '''
+    #fixtures = ['products_testdata.json']
+
+    def test_index(self):
+        '''
+        Testando a criao dos objetos com o metodo create do django.
+        '''
+        product_spec01 = ProductSpec.objects.create(
+            name = 'Camiseta')
+        product_spec02 = ProductSpec.objects.create(
+            name= 'Caixas')
+
+        assert product_spec01 is not None
+        assert product_spec02 is not None
+
+        product01 = Product.objects.create(
+            name = 'MegaBoga',
+            price = Decimal('23.20'),
+            product_spec = product_spec01)
+        
+        product02 = Product.objects.create(
+            name = 'Caixa Vazia',
+            price = '0.00',
+            product_spec = product_spec02)
+
+        assert product01 is not None
+        assert product02 is not None
+
+        feature01 = Feature.objects.create(
+            name = 'Cor',
+            description = 'Define a cor do produto',
+            product_spec = product_spec01)
+    
+        feature02 = Feature.objects.create(
+            name='Peso',
+            description = 'Define o peso do produto',
+            product_spec = product_spec02)
+
+        assert feature01 is not None
+        assert feature02 is not None        
+
+        feature_value01 = FeatureValue.objects.create(
+            value = 'Vermelho',
+            feature = feature01,
+            product = product01)
+        
+        feature_value02 = FeatureValue.objects.create(
+            value = '20kg',
+            feature = feature02,
+            product = product02)
+
+        assert feature_value01 is not None
+        assert feature_value02 is not None
+
+
+    def test_product_feature(self):
+        '''
+        As features_values definidos  para um produto devem pertencer a 
+        lista de features do produto_spec.
+        '''
+        
+        product01 = Product.objects.get(pk=2)
+        feature01 = Feature.objects.get(pk=1)
+        feature02 = Feature.objects.get(pk=2)
+
+        assert product01 is not None
+        assert feature01 is not None
+        assert feature02 is not None
+
+        self.assertTrue(product01.feature_is_valid(feature01))
+        self.assertTrue(product01.feature_is_valid(feature02))
+               
+
